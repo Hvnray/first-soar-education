970b6dfd1bbaff4050e92e3fcc3b09a5ca9267bf
hvnray <slenzy001@gmail.com>
2022-09-04 16:54:28 +0100

    fix google font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@700&family=Montserrat:wght@700;400;300&display=optional"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@700&family=Montserrat:wght@300;400;700&display=optional"
           rel="stylesheet"
         />
       </Head>
